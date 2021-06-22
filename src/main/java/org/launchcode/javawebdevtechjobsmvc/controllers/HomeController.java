dda7c8a673730aeafd77fb74ef0031432ee57cbf
hhantak <hank.hantak@outlook.com>
2021-06-21 20:04:45 -0500

    First and final commit. Thanks for the help, Theo!

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/HomeController.java
@@ -2,6 +2,7 @@ package org.launchcode.javawebdevtechjobsmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -17,12 +17,32 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "")
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+//    @RequestMapping(value = "search/results",  method = RequestMethod.POST)
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
+        ArrayList<Job> jobs;
+        if (searchTerm.equals("")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else if (searchType.toLowerCase().equals("all")){
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        //6. pass jobs into the search.html view via the model parameter
+        model.addAttribute("jobs", jobs);
+        //7. Pass ListController.columnChoices into the view (like line 22)
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
 }
